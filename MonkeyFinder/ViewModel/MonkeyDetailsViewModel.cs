058ad9f0f13a0d109119f470eca25fba90235c4b
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-15 13:37:26 -0300

    add navegation

--- MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -8,4 +8,10 @@ public partial class MonkeyDetailsViewModel : BaseViewModel
     public MonkeyDetailsViewModel()
     {
     }
+    
+    // [RelayCommand]
+    // private async Task GoBackAsync()
+    // {
+    //     await Shell.Current.GoToAsync("..");
+    // }
 }
